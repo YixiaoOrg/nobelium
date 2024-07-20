fd7f4d874a4deea2fc6b2e1f275353ca6e5a1de6
Yixiao <86191112+YixiaoOrg@users.noreply.github.com>
2024-07-21 07:48:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/YixiaoOrg',
+  socialLink: 'https://twitter.com/jintanorg',
   seo: {
     keywords: ['jintan','金坛','金坛区'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
